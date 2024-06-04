98e38267fb21e53641b73324ae1a26ac7989f526
taei <roeehql@naver.com>
2024-06-04 16:06:49 +0900

    modify: stack 페이지 수정

--- src/components/stack/Badge.tsx
+++ src/components/stack/Badge.tsx
@@ -2,17 +2,7 @@ import { BadgeState } from "./Stack";
 import style from "./Stack.module.sass";
 
 const Badge = ({ shape, name }: BadgeState) => {
-  return (
-    <>
-      {shape === "square" && <div className={style.square}>{name}</div>}
-      {shape === "ribbon" && (
-        <div className={style.ribbon}>
-          <i>{name}</i>
-        </div>
-      )}
-      {shape === "one" && <div className={style.one}>{name}</div>}
-    </>
-  );
+  return <div className={`${style[shape]}`}>{name}</div>;
 };
 
 export default Badge;
--- src/components/stack/Stack.tsx
+++ src/components/stack/Stack.tsx
@@ -15,6 +15,14 @@ const Stack = () => {
 
   return (
     <div className={style.stack_wrapper}>
+      <div className={style.greet_card}>
+        <h4>My Stack !!</h4>
+        <p>
+          프로젝트에서 기능 구현 경험이 있고,
+          <br />
+          공식문서에서 제공하는 기본 코드 이상을 활용한 적이 있는 기술들 입니다.
+        </p>
+      </div>
       <div className={style.item_flex}>
         {stackList.map((item) => (
           <Badge key={item.name} shape={item.shape} name={item.name} />
